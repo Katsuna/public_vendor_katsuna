6333d86183d19cd0a12d71ce4091fb5218eafeed
George G <kreach3r@users.noreply.github.com>
2016-06-09 03:33:29 +0300

    Build more packages

--- products/common.mk
+++ products/common.mk
@@ -21,3 +21,11 @@ PRODUCT_PACKAGE_OVERLAYS += vendor/katsuna/overlay/common
 # Enable SIP+VoIP on all targets
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.software.sip.voip.xml:system/etc/permissions/android.software.sip.voip.xml
+
+# Extra Packages
+PRODUCT_PACKAGES += \
+    CellBroadcastReceiver \
+    Launcher3 \
+    LiveWallpapersPicker \
+    Stk \
+    WallpaperPicker
