ce4ac8496e46fad4ff7398a0cbfa7f0177545359
George G <kreach3r@users.noreply.github.com>
2017-06-26 20:15:00 +0200

    Abandon default-permissions for now

--- products/common.mk
+++ products/common.mk
@@ -47,5 +47,5 @@ PRODUCT_COPY_FILES += \
     vendor/katsuna/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions
 
 # Default permission grant exceptions for Katsuna apps
-PRODUCT_COPY_FILES += \
-    vendor/katsuna/prebuilt/common/etc/default-permissions/katsuna-permissions.xml:system/etc/default-permissions/katsuna-permissions.xml
+#PRODUCT_COPY_FILES += \
+#    vendor/katsuna/prebuilt/common/etc/default-permissions/katsuna-permissions.xml:system/etc/default-permissions/katsuna-permissions.xml
