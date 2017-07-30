3d75866bb342964b8c186503f58a7ae9dbb07ed7
George G <kreach3r@users.noreply.github.com>
2017-07-30 18:26:56 +0200

    Add BUILD_TAG to zip name, too

--- rom_version.sh
+++ rom_version.sh
@@ -13,10 +13,13 @@ fi
 ROM_NAME=Katsuna_OS_$major.$feature.$bug-$(date +%Y%m%d)
 ROM_ZIP_NAME=Katsuna_OS_$device"_"$major.$feature.$bug-$(date +%Y%m%d)
 
-ROM_VERSION=$ROM_NAME
+
 if [ -n "$BUILD_TAG" ]; then
-    ROM_VERSION=$ROM_NAME-$BUILD_TAG
+    ROM_NAME=$ROM_NAME-$BUILD_TAG
+    ROM_ZIP_NAME=$ROM_ZIP_NAME-$BUILD_TAG
 fi
+
+ROM_VERSION=$ROM_NAME
 
 export ROM_ZIP_NAME
 export ROM_VERSION
