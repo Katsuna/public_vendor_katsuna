4e2df4eab343a36503d4cfb240be987da86f6d1e
Katsuna CI <ci@katsuna.com>
2018-10-02 20:11:58 +0200

    bullhead: Add oct18 fingerprints

--- products/katsuna_bullhead.mk
+++ products/katsuna_bullhead.mk
@@ -14,5 +14,5 @@ PRODUCT_MANUFACTURER := LGE
 # Fingerprint
 PRODUCT_BUILD_PROP_OVERRIDES += \
     PRODUCT_NAME=bullhead \
-    BUILD_FINGERPRINT="google/bullhead/bullhead:8.1.0/OPM6.171019.030.K1/4947289:user/release-keys" \
-    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM6.171019.030.K1 4947289 release-keys"
+    BUILD_FINGERPRINT="google/bullhead/bullhead:8.1.0/OPM7.181005.003/4984324:user/release-keys" \
+    PRIVATE_BUILD_DESC="bullhead-user 8.1.0 OPM7.181005.003 4984324 release-keys"
