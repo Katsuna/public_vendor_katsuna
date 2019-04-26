d51dfb55643aab570fd930808abdcc9627a51e09
invisiblek <dan.pasanen@gmail.com>
2019-04-26 18:48:49 +0300

    sepolicy: add rules for updater and update_engine
    * We need special exceptions for update_engine scripts because we
      handle backuptool operations through it.
    
    * priv_app needs to be able to make ota package dirs in order to
      put the ota in the correct spot.
    
    Change-Id: I42a421e4d84adde9514932a056c082d1cb3e09b4
    
    Adapted for our vendor_katsuna

--- sepolicy/sepolicy.mk
+++ sepolicy/sepolicy.mk
@@ -1,5 +1,8 @@
 # This policy configuration will be used by all products
 # Requires commit on platform/build
 
+BOARD_PLAT_PRIVATE_SEPOLICY_DIR += \
+    vendor/katsuna/sepolicy/private
+
 BOARD_SEPOLICY_DIRS += \
     vendor/katsuna/sepolicy
