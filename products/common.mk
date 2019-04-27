namdeptrai2003/android_vendor_dnd

cm: Put install tools in OUT/install
 * This allows us to avoid modifying /system at install time.

Change-Id: I747551d7f38a3eef70ab64f32b6d4a4749c01012

config/common.mk
@@ -81,15 +81,15 @@ PRODUCT_COPY_FILES += \
 # Backup Tool
 ifneq ($(WITH_GMS),true)
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
-    vendor/cm/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions \
+    vendor/cm/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/cm/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
     vendor/cm/prebuilt/common/bin/50-cm.sh:system/addon.d/50-cm.sh \
     vendor/cm/prebuilt/common/bin/blacklist:system/addon.d/blacklist
 endif
 
 # Signature compatibility validation
 PRODUCT_COPY_FILES += \
-    vendor/cm/prebuilt/common/bin/otasigcheck.sh:system/bin/otasigcheck.sh
+    vendor/cm/prebuilt/common/bin/otasigcheck.sh:install/bin/otasigcheck.sh
 
 # init.d support
 PRODUCT_COPY_FILES += \

Katsuna/public_vendor_katsuna

products/common.mk
@@ -67,8 +67,8 @@ PRODUCT_PACKAGES += \
 # Backuptool Support
 PRODUCT_COPY_FILES += \
     vendor/katsuna/prebuilt/common/addon.d/50-katsuna.sh:system/addon.d/50-katsuna.sh \
-    vendor/katsuna/prebuilt/common/bin/backuptool.sh:system/bin/backuptool.sh \
-    vendor/katsuna/prebuilt/common/bin/backuptool.functions:system/bin/backuptool.functions
+    vendor/katsuna/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
+    vendor/katsuna/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
 ifeq ($(AB_OTA_UPDATER),true)
 PRODUCT_COPY_FILES += \